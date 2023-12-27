79d5e8fb413d0996eddd54ccfa37a67650e15186
akinbola247 <akinbolakehinde247@gmail.com>
2023-12-27 12:34:14 +0100

    frontend update

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import Hero from '../components/Hero'
+import Howtoplay from '../components/Howtoplay'
+import Upcoming from '../components/Upcoming'
+import Newsletter from '../components/Newsletter'
+import Company from '../components/Company'
 
 const Home = () => {
   return (
     <Layout>
         <Hero />
-        <div>Landing Page</div>
+        <Howtoplay />
+        <Upcoming />
+        <Newsletter />
+        <Company />
     </Layout>
   )
 }
